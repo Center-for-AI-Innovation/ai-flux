085614250f7c949dde01848dd0986a439c426389
Allen <jallen17@cc-login2.campuscluster.illinois.edu>
2025-09-26 16:00:41 -0500

    Refactored the ollama job composer to it's own module

--- src/aiflux/slurm/__init__.py
+++ src/aiflux/slurm/__init__.py
@@ -1,5 +1,14 @@
 """SLURM integration for AI-Flux."""
 
 from .runner import SlurmRunner
+from .batch_scripts import create_ollama_batch_script
+from .batch_scripts import create_vllm_batch_script
 
-__all__ = ['SlurmRunner'] 
+__all__ = [
+    # runner
+    'SlurmRunner',
+
+    # script composers
+    'create_ollama_batch_script',
+    'create_vllm_batch_script'
+] 
