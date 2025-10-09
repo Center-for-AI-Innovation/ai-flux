e1f46c784de8ceee5fc7d6e0d48eeea7988194fe
Joshua Allen <jallen17@illinois.edu>
2025-10-09 14:00:15 -0500

    bug fix on that line again

--- src/aiflux/slurm/runner.py
+++ src/aiflux/slurm/runner.py
@@ -325,7 +325,8 @@ class SlurmRunner:
             logger.error("Unknown engine choice: {}".format(self.engine))
             raise NotImplementedError
 
-        logger.info(f"Job script:\n{'\n'.join(job_script)}")
+        job_script_str = "\n".join(job_script)
+        logger.info(f"Job script: {job_script_str}")
         # Write job script
         job_script_path = self.workspace / "job.sh"
         try:
